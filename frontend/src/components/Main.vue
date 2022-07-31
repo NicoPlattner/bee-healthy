de9946f54af737472380248a941c96fe3934e7b1
Nico <nico.plattner@tuwien.ac.at>
2022-07-31 06:07:51 +0200

    frontend overview done, backend has gitignore now

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,29 +1,28 @@
 <template>
-  <div>
-    <HeaderBar></HeaderBar>
+	<div>
+		<HeaderBar></HeaderBar>
 
-    <div id="bodycontent">
-      <b-container>
-        <b-row>
-          <form>
-            <div class="form-group">
-              <label for="exampleFormControlSelect1"></label>
-              <select v-model="selected" class="form-control" id="exampleFormControlSelect1" v-if="hives.length > 0">
-                <option>All Hives</option>
-                <option>Beehive 1</option>
-                <option>Beehive 2</option>
-                <option>Beehive 3</option>
-              </select>
-            </div>
-          </form>
-        </b-row>
+		<div id="bodycontent">
+			<b-container>
+				<b-row>
+					<form>
+						<div class="form-group">
+							<label for="exampleFormControlSelect1"></label>
+							<select v-model="selected" class="form-control" id="exampleFormControlSelect1" v-if="hives.length > 0">
+								<option>All Hives</option>
+								<option v-for="hive in this.hives" v-bind:key="hive.id">
+									{{ hive.name }}
+								</option>
+							</select>
+						</div>
+					</form>
+				</b-row>
 
-        <OverviewPage v-if="this.selected === 'All Hives'"></OverviewPage>
-        <HivePage v-if="this.selected !== 'All Hives'"></HivePage>
-      </b-container>
-    </div>
-  </div>
-
+				<OverviewPage v-if="this.selected === 'All Hives'"></OverviewPage>
+				<HivePage v-if="this.selected !== 'All Hives'"></HivePage>
+			</b-container>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -31,42 +30,41 @@ import HeaderBar from "@/layout/Header";
 import {Weather} from "@/enums/weather.enum";
 import OverviewPage from "@/components/OverviewPage";
 import HivePage from "@/components/HivePage";
+
 export default {
-  name: "MainScreen",
-  components: {HivePage, OverviewPage, HeaderBar},
-  data() {
-    return {
-      selected: 'All Hives',
-      hives: [],
-      temperature: 0,
-      weather: Weather.Sunny
-    }
-  },
-  mounted() {
-    this.temperature = 36;
-    this.weather = Weather.LightClouds;
+	name: "MainScreen",
+	components: {HivePage, OverviewPage, HeaderBar},
+	data() {
+		return {
+			selected: 'All Hives',
+			hives: [],
+			temperature: 0,
+			weather: Weather.Sunny
+		}
+	},
+	mounted() {
+		this.temperature = 36;
+		this.weather = Weather.LightClouds;
 
-    fetch('http://localhost:5000/getAllHives/0')
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-
-
-  }
+		fetch('http://localhost:5000/getAllHives/0')
+		.then((response) => response.json())
+		.then((data) => this.hives = data);
+	}
 }
 </script>
 
 <style scoped>
 #bodycontent {
-  padding: 0 2em;
+	padding: 0 2em;
 }
 
 select {
-  box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
+	box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
 }
 
 
 .row {
-  margin-bottom: 2em;
+	margin-bottom: 2em;
 }
 
 </style>
--- frontend/src/components/OverviewPage.vue
+++ frontend/src/components/OverviewPage.vue
@@ -1,15 +1,112 @@
 <template>
-  <div>
-ov
-  </div>
+	<div>
+		<b-row style="height: 3em" v-if="problemHives.length === 0 && downHives.length === 0">
+			<b-col cols="12">
+				<div class='square-box'>
+					<div style="background-color: green" class='square-content'>
+						<div class="app-content">
+							No problems
+						</div>
+					</div>
+				</div>
+			</b-col>
+		</b-row>
+
+		<div v-if="problemHives.length !== 0">
+			<b-row style="height: 3em; margin-bottom: 5em" v-for="hive in problemHives" v-bind:key="hive.id">
+				<b-col cols="12">
+					<div class='square-box'>
+						<div style="background-color: orangered" class='square-content'>
+							<div class="app-content">
+								Health of {{ hive.name }} critical!
+							</div>
+						</div>
+					</div>
+				</b-col>
+			</b-row>
+		</div>
+
+		<div v-if="downHives.length !== 0">
+			<b-row style="height: 3em; margin-bottom: 5em" v-for="hive in downHives" v-bind:key="hive.id">
+				<b-col cols="12">
+					<div class='square-box'>
+						<div style="background-color: darkgray" class='square-content'>
+							<div class="app-content">
+								{{ hive.name }} is down
+							</div>
+						</div>
+					</div>
+				</b-col>
+			</b-row>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "OverviewPage"
+	name: "OverviewPage",
+	data() {
+		return {
+			hives: [],
+			problemHives: [],
+			downHives: []
+		}
+	},
+	mounted() {
+		fetch('http://localhost:5000/getAllHives/0')
+		.then((response) => response.json())
+		.then((data) => {
+			this.hives = data;
+
+			this.hives.forEach(hive => {
+				if(hive.sensorData.predictedHealthStatus[hive.sensorData.predictedHealthStatus.length - 1] < 50) {
+					this.problemHives.push(hive);
+				}
+			})
+
+			this.hives.forEach(hive => {
+				const sensorTime = hive.sensorData.epochTime[hive.sensorData.epochTime.length - 1];
+				if(sensorTime > Math.floor(new Date().getTime() / 1000) + 7200000) { //2 hours difference
+					this.downHives.push(hive);
+				}
+			})
+		});
+	}
 }
 </script>
 
 <style scoped>
+.app-content {
+	margin-top: .5em;
+	font-size: .6em;
+	text-align: center;
+}
 
+.square-box {
+	position: relative;
+	overflow: hidden;
+	width: 100%;
+	margin-right: 10%;
+	background: #FFCD32;
+	border-radius: .7em;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+}
+
+.square-box:before {
+	content: "";
+	display: block;
+	padding-top: 30%;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+}
+
+.square-content {
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	color: black;
+	font-size: 2.5em;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+}
 </style>
